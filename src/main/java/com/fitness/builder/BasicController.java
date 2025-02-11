0827262ce14685d038268d2b807b04b3d397d0a1
pdy1207 <pdyme1207@gmail.com>
2025-02-11 14:44:45 +0900

    🪄 thymeleaf / mysql connetor / lombok / security setting

---
+++ src/main/java/com/fitness/builder/BasicController.java
@@ -0,0 +1,13 @@
+package com.fitness.builder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+    String hello(){
+        return "index.html";
+    }
+}
